462db72c1aaef9da56710a9a302a5b282c90bfcc
jonasstibolt <jonasholmgaard@gmail.com>
2024-03-21 17:59:01 +0100

    fix error by installing app

--- J/lecture 3/DJANGO/bday/views.py
+++ J/lecture 3/DJANGO/bday/views.py
@@ -6,5 +6,11 @@ import datetime
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": now.month.day
+        "newyear": now.day == 23 and now.month == 1
     })
+
+# Maybe you can make this with a if/else conditional instead?
+
+
+def index(request):
+    return render(request, "bday/index.html")
